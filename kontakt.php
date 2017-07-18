052e6c9aa663c8d6d487baa99574324ce001e977
apyd <arturpydych@gmail.com>
2017-07-18 09:36:20 +0200

    Form improvements
    - Styled mail as html
    - Added other information to mail body

--- kontakt.php
+++ kontakt.php
@@ -2,11 +2,14 @@
 $to      = 'arturpydych@gmail.com';
 $name	 = $_POST['name'];
 $email   = $_POST['email'];
+$from    = $email;
 $subject = 'Cullera - kontakt - ' . $name ;
-$message = $_POST['message'] . "\r\n";
-$headers = 'Od: ' . $name . "\r\n" ;
-$headers = 'MIME-Version: 1.0'."\r\n";
-$headers .= 'Content-type: text/plain; charset=iso-8859-2'."\r\n";	
+$content = nl2br($_POST['content']);
+$message = '<b>Imię i nazwisko:   </b>' .$name. '<br><b>Od:  </b> '.$email. '<p>' .$content . '</p>';
 
-mail($to, $subject, $message , $headers);
+$headers = 'Od: ' . $from . "\r\n" ;
+$headers .= 'MIME-Version: 1.0'."\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8'."\r\n";	
+
+mail($to, $subject, $message, $headers);
 ?>
