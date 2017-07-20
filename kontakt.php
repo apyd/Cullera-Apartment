23396255b035439f1fc27e65d20c2e81fdead237
apyd <arturpydych@gmail.com>
2017-07-20 10:05:48 +0200

    Form improvements
    - added missing closing tags for span
    - added all fields to mail body

--- kontakt.php
+++ kontakt.php
@@ -5,7 +5,7 @@ $email   = $_POST['email'];
 $from    = $email;
 $subject = 'Cullera - kontakt - ' . $name ;
 $content = nl2br($_POST['content']);
-$message = '<b>Imię i nazwisko:   </b>' .$name. '<br><b>Od:  </b> '.$email. '<p>' .$content . '</p>';
+$message = '<b>Imię i nazwisko:   </b>' .$name. '<br><b>Od:  </b> '.$email. ' <br> <b>Treść wiadomości:  </b>' . '<br> <p>' .$content . '</p>';
 
 $headers = 'Od: ' . $from . "\r\n" ;
 $headers .= 'MIME-Version: 1.0'."\r\n";
