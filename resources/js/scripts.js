024188cc6c5290756041eec2a7e857797e45e015
Michal Wajer <mikail94@gmail.com>
2017-08-03 18:44:23 +0200

    Fixed bugs with semantic tags
    Fixed bugs with semantic tags where some tabs haven't been opening properly

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,69 +1,72 @@
-$(document).ready(function () {
-  if($('#txtCaptcha').length > 0) {
-  var a = Math.ceil(Math.random() * 9)+ '';
-  var b = Math.ceil(Math.random() * 9)+ '';
-  var c = Math.ceil(Math.random() * 9)+ '';
-  var d = Math.ceil(Math.random() * 9)+ '';
-  var e = Math.ceil(Math.random() * 9)+ '';
-  var code = a + b + c + d + e;
-  document.getElementById("txtCaptcha").value = code;
-  document.getElementById("CaptchaDiv").innerHTML = code;
+$(document).ready(function() {
+  if ($('#txtCaptcha').length > 0) {
+    var a = Math.ceil(Math.random() * 9) + '';
+    var b = Math.ceil(Math.random() * 9) + '';
+    var c = Math.ceil(Math.random() * 9) + '';
+    var d = Math.ceil(Math.random() * 9) + '';
+    var e = Math.ceil(Math.random() * 9) + '';
+    var code = a + b + c + d + e;
+    document.getElementById("txtCaptcha").value = code;
+    document.getElementById("CaptchaDiv").innerHTML = code;
   }
-  if($('#date-arrive').length > 0) {
-  var time = new Date();
-  var year = time.getFullYear().toString();
-  var month = (time.getMonth()+1).toString();
-  var day = time.getDate().toString();
-  if (month.length == 1) {month = "0" + month;}
-  if (day.length == 1) {day = "0" + day;}
-  var date = year + "-" + month + "-" + day;
-  document.getElementById('date-arrive').min = date;
-  document.getElementById('date-arrive').placeholder = document.getElementById('date-arrive').min;
-  document.getElementById('date-go').placeholder = document.getElementById('date-arrive').min;
+  if ($('#date-arrive').length > 0) {
+    var time = new Date();
+    var year = time.getFullYear().toString();
+    var month = (time.getMonth() + 1).toString();
+    var day = time.getDate().toString();
+    if (month.length == 1) {
+      month = "0" + month;
+    }
+    if (day.length == 1) {
+      day = "0" + day;
+    }
+    var date = year + "-" + month + "-" + day;
+    document.getElementById('date-arrive').min = date;
+    document.getElementById('date-arrive').placeholder = document.getElementById('date-arrive').min;
+    document.getElementById('date-go').placeholder = document.getElementById('date-arrive').min;
   }
-//Script for full calendar
-$('#calendar').fullCalendar({
+  //Script for full calendar
+  if ($('#date-arrive').length > 0) {
+    $('#calendar').fullCalendar({
 
-  header: {
-    left: 'prev,next today',
-    center: 'title',
-    right: 'month,listYear'
-  },
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,listYear'
+      },
 
-  displayEventTime: false, // don't show the time column in list view
+      displayEventTime: false, // don't show the time column in list view
 
-  // THIS KEY WON'T WORK IN PRODUCTION!!!
-  // To make your own Google API key, follow the directions here:
-  // http://fullcalendar.io/docs/google_calendar/
-  googleCalendarApiKey: 'AIzaSyBPciQcNvyG04-L9XwZq_JTUVrk1tgQfhY',
+      // THIS KEY WON'T WORK IN PRODUCTION!!!
+      // To make your own Google API key, follow the directions here:
+      // http://fullcalendar.io/docs/google_calendar/
+      googleCalendarApiKey: 'AIzaSyBPciQcNvyG04-L9XwZq_JTUVrk1tgQfhY',
 
-  // US Holidays
-  events: 'cullera.apartment@gmail.com',
+      // US Holidays
+      events: 'cullera.apartment@gmail.com',
 
-  eventClick: function(event) {
-    // opens events in a popup window
-    window.open(event.url, 'gcalevent', 'width=700,height=600');
-    return false;
-  },
+      eventClick: function(event) {
+        // opens events in a popup window
+        window.open(event.url, 'gcalevent', 'width=700,height=600');
+        return false;
+      },
 
-  loading: function(bool) {
-    $('#loading').toggle(bool);
+      loading: function(bool) {
+        $('#loading').toggle(bool);
+      }
+
+    });
   }
-
-});
 
 });
 
 function InvalidName(textbox) {
   var re = /[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
-  if(textbox.value == '') {
+  if (textbox.value == '') {
     textbox.setCustomValidity('Proszę podać imię i nazwisko');
-  }
-  else if (textbox.value.search(re) > -1)
-  {
+  } else if (textbox.value.search(re) > -1) {
     textbox.setCustomValidity('Proszę podać właściwe imię i nazwisko');
-  }
-  else {
+  } else {
     textbox.setCustomValidity('');
   }
   return true;
@@ -71,16 +74,13 @@ function InvalidName(textbox) {
 
 function InvalidEmail(textbox) {
   var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+$/;
-  if(textbox.value === '') {
+  if (textbox.value === '') {
     textbox.setCustomValidity('Proszę wprowadzić adres email');
-  }
-  else if (!textbox.value.match(re)) {
+  } else if (!textbox.value.match(re)) {
     textbox.setCustomValidity('Proszę wprowadzić poprawny adres email');
-  }
-  else if (textbox.value[textbox.value.length-1] === '.') {
+  } else if (textbox.value[textbox.value.length - 1] === '.') {
     textbox.setCustomValidity('Adres email nie może się kończyć kropką')
-  }
-  else {
+  } else {
     textbox.setCustomValidity("");
   }
   return true;
@@ -88,10 +88,9 @@ function InvalidEmail(textbox) {
 
 
 function InvalidText(textbox) {
-  if(textbox.value === '') {
+  if (textbox.value === '') {
     textbox.setCustomValidity('Wprowadź tekst');
-  }
-  else {
+  } else {
     textbox.setCustomValidity("");
   }
   return true;
@@ -99,13 +98,11 @@ function InvalidText(textbox) {
 
 function InvalidTel(textbox) {
   var re = /\+([0-9]){11}/
-  if(textbox.value === '') {
+  if (textbox.value === '') {
     textbox.setCustomValidity('Proszę podać numer telefonu');
-  }
-  else if (!textbox.value.match(re)) {
+  } else if (!textbox.value.match(re)) {
     textbox.setCustomValidity('Proszę podać poprawny numer telefonu');
-  }
-  else {
+  } else {
     textbox.setCustomValidity("");
   }
   return true;
@@ -113,17 +110,15 @@ function InvalidTel(textbox) {
 
 function InvalidDateArrive(textbox) {
   var date = textbox.value.split("-");
-  var asked_date = new Date(date[0],date[1],date[2]);
+  var asked_date = new Date(date[0], date[1], date[2]);
   var date_min = document.getElementById('date-arrive').min.split("-");
   var min_date = new Date(date_min[0], date_min[1], date_min[2]);
-//  alert(typeof(parseInt(test[0])));
+  //  alert(typeof(parseInt(test[0])));
   if (textbox.value == "") {
     textbox.setCustomValidity("Proszę podać datę");
-  }
-  else if(asked_date < min_date) {
+  } else if (asked_date < min_date) {
     textbox.setCustomValidity("Proszę podać właściwą datę");
-  }
-  else {
+  } else {
     textbox.setCustomValidity("");
     document.getElementById('date-go').min = document.getElementById('date-arrive').value;
     document.getElementById('date-go').placeholder = document.getElementById('date-arrive').value;
@@ -133,55 +128,52 @@ function InvalidDateArrive(textbox) {
 
 function InvalidDateGo(textbox) {
   var date = textbox.value.split("-");
-  var asked_date = new Date(date[0],date[1],date[2]);
+  var asked_date = new Date(date[0], date[1], date[2]);
   var date_min = document.getElementById('date-go').min.split("-");
   var min_date = new Date(date_min[0], date_min[1], date_min[2]);
-//  alert(typeof(parseInt(test[0])));
+  //  alert(typeof(parseInt(test[0])));
   if (textbox.value == "") {
     textbox.setCustomValidity("Proszę podać datę");
-  }
-  else if(asked_date < min_date) {
+  } else if (asked_date < min_date) {
     textbox.setCustomValidity("Proszę podać właściwą datę");
-  }
-  else {
+  } else {
     textbox.setCustomValidity("");
   }
   return true;
 };
 
 // Validate input against the generated number
-function ValidCaptcha(){
+function ValidCaptcha() {
   var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
   var str2 = removeSpaces(document.getElementById('CaptchaInput').value);
-  if (str1 == str2){
-    if ( $( "input:first" ).val() !== "" ) {
-      $( "#messageSent").css("display","block");
-      $( "#messageSent").css("opacity","1");
-      $( "#submit-button").focus();
+  if (str1 == str2) {
+    if ($("input:first").val() !== "") {
+      $("#messageSent").css("display", "block");
+      $("#messageSent").css("opacity", "1");
+      $("#submit-button").focus();
     }
-    var a = Math.ceil(Math.random() * 9)+ '';
-    var b = Math.ceil(Math.random() * 9)+ '';
-    var c = Math.ceil(Math.random() * 9)+ '';
-    var d = Math.ceil(Math.random() * 9)+ '';
-    var e = Math.ceil(Math.random() * 9)+ '';
+    var a = Math.ceil(Math.random() * 9) + '';
+    var b = Math.ceil(Math.random() * 9) + '';
+    var c = Math.ceil(Math.random() * 9) + '';
+    var d = Math.ceil(Math.random() * 9) + '';
+    var e = Math.ceil(Math.random() * 9) + '';
     var code = a + b + c + d + e;
     document.getElementById("txtCaptcha").value = code;
     document.getElementById("CaptchaDiv").innerHTML = code;
     $("#error").text('');
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 };
 // Remove the spaces from the entered and generated code
-function removeSpaces(string){
+function removeSpaces(string) {
   return string.split(' ').join('');
 };
 
 function close_modal() {
-$( "#messageSent").css("display","none");
-document.getElementById("name").value = "";
-document.getElementById("email").value = "";
-document.getElementById("comment").value = "";
+  $("#messageSent").css("display", "none");
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("comment").value = "";
 };
